ef2bb173f81fe87eab0d9c0a6057b594155c0b1d
Alessandro Pasin <alepasin3@gmail.com>
2025-10-06 20:00:14 +0200

    feat: added Author entity

---
+++ src/main/java/com/example/library/entity/Author.java
@@ -0,0 +1,36 @@
+package com.example.library.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "authors")
+public class Author extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String firstName;
+
+
+    @Column(nullable = false, length = 255)
+    private String lastName;
+
+    @Column(nullable = false)
+    private LocalDateTime birthDate;
+}
